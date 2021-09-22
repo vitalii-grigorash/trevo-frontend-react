e84d5f9866f963671ce90b47f3531b12011a5b05
st806853 <v.grigorash@spbu.ru>
2021-09-22 15:56:07 +0300

    TREVO-19 created GroupList component with group search

---
+++ src/components/GroupList/GroupList.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+function GroupList(props) {
+
+    const {
+        groupsData
+    } = props;
+
+    const [isGroupSelectContainerOpen, setGroupSelectContainerOpen] = useState(false);
+    const [groupName, setGroupName] = useState('Выберите группу');
+    const [isGroupSelected, setGroupSelected] = useState(false);
+
+    const selectData = (groupName) => {
+        setGroupName(groupName);
+        setGroupSelected(true);
+    }
+
+    function handleGroupSelectContainerOpen() {
+        if (isGroupSelectContainerOpen) {
+            setGroupSelectContainerOpen(false);
+        } else {
+            setGroupSelectContainerOpen(true);
+        }
+    }
+
+    return (
+        <div className="group-list">
+            <p className="group-list__heading">Список групп</p>
+            <div className="group-list__search-container">
+                <div className="group-list__select-group-container" onClick={handleGroupSelectContainerOpen}>
+                    <p className={`group-list__select-group-text ${isGroupSelected && 'group-list__select-group-text_active'}`}>{groupName}</p>
+                    <div className="group-list__select-group-arrow" />
+                    {isGroupSelectContainerOpen && (
+                        <div className="group-list__group-select-container">
+                            {groupsData.length === 0 ? (
+                                <div className="group-list__group-select-group-container">
+                                    <p className="group-list__group-select-group-text">Необходимо добавить группу</p>
+                                </div>
+                            ) : (
+                                groupsData.map((data, index) => (
+                                    <div key={index} className="group-list__group-select-group-container group-list__group-select-group-container_result" onClick={() => selectData(data.groupName)}>
+                                        <p className="group-list__group-select-group-text">{data.groupName}</p>
+                                    </div>
+                                ))
+                            )}
+                        </div>
+                    )}
+                </div>
+                <button className={`group-list__select-group-button ${isGroupSelected && 'group-list__select-group-button_active'}`} type="button">Поиск</button>
+            </div>
+        </div>
+    );
+
+}
+
+export default GroupList;
--- src/components/MyListVagonsTab/MyListVagonsTab.js
+++ src/components/MyListVagonsTab/MyListVagonsTab.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import GroupList from '../GroupList/GroupList';
 
 function MyListVagonsTab() {
 
+    const [groupsData, setGroupsData] = useState([
+        {
+            groupName: 'Группа 4546',
+            vagonsValue : '5 634'
+        },
+        {
+            groupName: 'Группа 34356',
+            vagonsValue : '35 634'
+        },
+        {
+            groupName: 'Группа 426',
+            vagonsValue : '34'
+        },
+        {
+            groupName: 'Группа 678765',
+            vagonsValue : '355'
+        },
+    ]);
+
     return (
         <div className="my-list-vagons-tab">
-            <h2 className="my-list-vagons-tab__heading">My List Vagons Tab</h2>
+            <GroupList
+                groupsData={groupsData}
+            />
         </div>
     );
 
