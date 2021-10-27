f91340124ccbc9652dc3040f5091f04a9334e53b
Vitalii Grigorash <v.grigorash@spbu.ru>
2021-10-27 18:54:09 +0300

    TREVO-77 added first table Carriage data

--- src/components/SummaryDislocationTable/SummaryDislocationTable.js
+++ src/components/SummaryDislocationTable/SummaryDislocationTable.js
@@ -202,6 +202,93 @@ function SummaryDislocationTable() {
                                 <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_cargo-weight-kg">0</p>
                             </div>
                         </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_loaded-mileage-km">Пробег в груженом состоянии (км)</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_loaded-mileage-km">4170</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_empty-mileage-km">Пробег в порожнем состоянии (км)</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_empty-mileage-km">3311</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_total-mileage-km">Пробег общий (км)</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_total-mileage-km">7481</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_standard-mileage-km">Нормативная величина пробега (км)</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_standard-mileage-km">160000</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_remaining-mileage-km">Остаток пробега (км)</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_remaining-mileage-km">152519</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_mileage-sign">Признак пробега</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_mileage-sign">152519</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_special-marks">Особые отметки</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_special-marks">3, 8, 0</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_previously-unloaded-cargo">Ранее выгруженный груз</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_previously-unloaded-cargo">Мазут топочный (221066)</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="summary-dislocation-table__table">
+                    <div className="summary-dislocation-table__table-heading-container">
+                        <p className="summary-dislocation-table__table-heading">Дислокация вагона</p>
+                    </div>
+                    <div className="summary-dislocation-table__table-columns-container">
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_operation-station">Станция операции</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_operation-station">ЛЯМЦЕВО (071716)</p>
+                            </div>
+                        </div>
+                        <div className="summary-dislocation-table__table-column">
+                            <div className="summary-dislocation-table__table-column-heading-container">
+                                <p className="summary-dislocation-table__table-column-heading summary-dislocation-table__table-column-heading_operation-road">Дорога операции</p>
+                            </div>
+                            <div className="summary-dislocation-table__table-row-container">
+                                <p className="summary-dislocation-table__table-row-text summary-dislocation-table__table-row-text_operation-road">ОКТЯБРЬСКАЯ (01)</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
